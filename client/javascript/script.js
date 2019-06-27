93006795da7870bdfa5ca51531707de35f8fe390
Maksura Alam <maksura.alam@gmail.com>
2019-06-27 17:32:08 +0700

    Added Country sidebar

--- client/javascript/script.js
+++ client/javascript/script.js
@@ -1,14 +1,27 @@
 //await for window load
 window.onload = () => {
 	//let sidebar = document.getElementById('sidebar')
-	let ul = document.getElementsByTagName('ul')[0]
-	console.log(ul)
+	let ul_city = document.getElementById('city')
+	console.log(ul_city)
 	let cities = ['Paris', 'New York', 'Sydney', 'Cape Town']
 	cities.forEach((c) => {
-		ul.insertAdjacentHTML('beforeEnd', `
+		ul_city.insertAdjacentHTML('beforeEnd', `
 		<li>
-		 <a href ="#"> ${c}</a>
-		</li>`
-	)
+		 <a href="#"> ${c}</a>
+		</li>
+	`)
 	})
+
+
+let ul_country = document.getElementById('country')
+console.log(ul_country)
+let countries = ['France', 'USA', 'South Africa', 'Asutralia']
+countries.forEach((c) => {
+	ul_country.insertAdjacentHTML('beforeEnd',`
+<li>
+ <a href ="#"> ${c} </a>
+</li>
+ `)
+})
+
 }
