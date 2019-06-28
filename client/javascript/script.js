cfd7562ac9bdb60b88db340dcfa323f7ddfac0db
Maksura Alam <maksura.alam@gmail.com>
2019-06-28 15:20:31 +0700

    Added Products and Countries from Api

--- client/javascript/script.js
+++ client/javascript/script.js
@@ -1,18 +1,64 @@
-window.onload =() = {
-//homes
 
-axios.get(/api/homes').then((res) => {
+window.onload = () => {
+
+axios.get('/api/homes').then((res) => {
 	// console.log('res', res.data)
-	let homes = res.data
-	// target products
-	let homes_ui = document.getElementById('products')
+	let homes = res.data // the resulting data from homes
 
-	// display each product in the DOM
-	products.forEach((p) => {
-		products_ui.insertAdjacentHTML('beforeEnd', `
-		<div class="product">
-			<div class="product-image">
-				<i class="far fa-star"></i>
+	//target homes
+	let homes_ui = document.getElementById('properties')
+
+	//display homes in DOM
+	homes.forEach((e) => {
+		homes_ui.insertAdjacentHTML('afterbegin', `
+		<div class="card">
+			<img src="${e.images}" alt="">
+			<h5> ${e.name}</h5>
+			<p>Price: ${e.price}</p>
+			<p><i class="fas fa-star"></i></p>
+		</div>
+		`
+	)
+	})
+})//end of all homes
 
 
-}
+//create sidebar for country filter
+axios.get('/api/countries').then((res)=> {
+	// console.log('res', res.data)
+	let countries = res.data// get resulting countries_get
+
+	//target countries_get
+	let countries_ui = document.getElementById('country')
+
+	//display countries in UI
+	countries.forEach((e) => {
+		countries_ui.insertAdjacentHTML('afterbegin', `
+		<li id="${e.id}" class="country"> ${e.name}</li>
+		`)
+	})
+})// end of countries list on sidebar
+
+//create sidebar for city Filter
+axios.get('/api/cities').then((res) => {
+	// console.log('res', res.data)
+	let cities = res.data//get resulting cities_get
+
+	//target countries_get
+	let cities_ui = document.getElementById
+})
+
+
+
+//create an eventlister for Australia
+document.addEventListener('click', (e) => {
+	// console.log("helo World")
+	if (e.target.classList.contains('country')) {
+		// console.log(e.target.id)//
+		//get target categories
+		axios.get('/api/')
+	}
+})
+
+
+}///end of window load
